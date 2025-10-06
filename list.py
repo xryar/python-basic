4f6c023937acd234fb1db18982a4bf58ded36be6
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-10-06 15:18:39 +0700

    python list part 2

--- list.py
+++ list.py
@@ -37,4 +37,27 @@ for row in matrix:
         print(cel, end=" ")
     print()
 
-# convert range to list
+# convert to list
+
+# range to list
+range_1 = range(0, 10)
+list_1 = list(range_1)
+print(list_1)
+
+range_2 = range(0, 22, 3)
+list_2 = list(range_2)
+print(list_2)
+
+range_3 = range(100, 0, -10)
+list_3 = list(range_3)
+print(list_3)
+
+# string to list
+alphabets = "abcdefghijk" 
+list_4 = list(alphabets)
+print(list_4)
+
+# tuple to list
+tuple_1 = (1, 2, 3, 4)
+list_5 = list(tuple_1)
+print(list_5)
